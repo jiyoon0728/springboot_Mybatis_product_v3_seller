9d5bd2a54e79f5635e6a1963bb73f03a219952e7
ji yoon <wldbf11111@naver.com>
2023-04-12 21:58:01 +0900

    product프로젝트 생성 및 초기셋팅 완료

---
+++ src/main/java/shop/mtcoding/product/Home.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
